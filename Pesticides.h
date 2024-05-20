d726fd4eba47cfd3ddc3af449b4edc6c0617dd5a
Fr4g3z <Fr4g3z@DESKTOP-4AETDI8>
2024-05-20 07:44:08 +0200

    Added 3 subclasses for Pesticides

--- Pesticides.h
+++ Pesticides.h
@@ -3,14 +3,18 @@
 #include <iostream>
 
 class Pesticides {
+protected:
 	std::string name;
 	int quantity;
 	std::string target;
+	std::string application_type;
 public:
-	Pesticides(): name(), quantity(), target() {}
-	Pesticides(std::string _name, int _quantity, std::string _target) :
-		name(_name), quantity(_quantity), target(_target) {}
+	//Constructors
+	Pesticides(): name(), quantity(), target(), application_type() {}
+	Pesticides(std::string _name, int _quantity, std::string _target, std::string _application_type) :
+		name(_name), quantity(_quantity), target(_target), application_type(_application_type) {}
 
+	//Setters and getters
 	void setName(std::string _name) { name = _name; }
 	std::string getName() const { return name; }
 
@@ -20,12 +24,39 @@ public:
 	void setTarget(std::string _target) { target = _target; }
 	std::string getTarget() const { return target; }
 
+	void setApplicationType(std::string _application_type) { application_type = _application_type; }
+	std::string getApplicationType() const { return application_type; }
+
+	//Output all fields
 	void displayInfo() {
 		std::cout << "Name: " << getName() << std::endl;
 		std::cout << "Quantity: " << getQuantity() << std::endl;
 		std::cout << "Target: " << getTarget() << std::endl;
+		std::cout << "Application Type: " << getApplicationType() << std::endl;
 	}
 
-	virtual std::string apply() const = 0;
+	virtual void apply() const = 0;
 	virtual ~Pesticides() {}
+};
+
+class Herbicide : public Pesticides {
+public:
+	Herbicide(std::string _name, int _quantity) : Pesticides(_name, _quantity, "Weeds", "Spraying") {}
+	void apply() const override {
+		std::cout << application_type << name << " to decrease ammount of unwanted " << target << std::endl;
+	}
+};
+
+class Insecticide : public Pesticides {
+	Insecticide(std::string _name, int _quantity) : Pesticides(_name, _quantity, "Insects", "Baiting") {}
+	void apply() const override {
+		std::cout << application_type << name << " to decrease ammount of unwanted " << target << std::endl;
+	}
+};
+
+class Fungicide : public Pesticides {
+	Fungicide(std::string _name, int _quantity) : Pesticides(_name, _quantity, "Fungal diseases", "Soil drenching") {}
+	void apply() const override {
+		std::cout << application_type << name << " to decrease ammount of unwanted " << target << std::endl;
+	}
 };
